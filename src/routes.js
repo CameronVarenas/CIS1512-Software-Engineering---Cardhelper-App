4baf235e9d9cd5fd9a76c3918dc31fa8b65ec223
Cameron Varenas <camerontvarenas@gmail.com>
2025-10-08 16:10:39 -0400

    fix invalid named import in routes.js
    import 'Switch' from react-router-dom replaced with 'Routes'

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Landing, Auth, UserDecks, Flashcard, AddCardsToDeck, CardList } from './components';
 
 export default (
-    <Switch>
+    <Routes>
         <Route exact path='/' component={Landing} />
         <Route path='/auth' component={Auth} />
         <Route exact path='/user-decks' component={UserDecks} />
         <Route exact path='/flashcard/:deckId' component={Flashcard} />
         <Route exact path='/add-card/:deckId' component={AddCardsToDeck} />
         <Route exact path='/card-list/:deckId' component={CardList} />
-    </Switch>
+    </Routes>
 )
