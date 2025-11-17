69d59317099b8fea69e8993b42fb2f46708cb6e4
Cameron Varenas <camerontvarenas@gmail.com>
2025-11-17 12:17:38 -0500

    change app.js file from function component to class component, create logout and componentDidMount functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
+import React, { Component } from 'react';
 import { HashRouter, Link } from 'react-router-dom';
 import routes from './routes';
 import './App.css';
+import { connect } from 'react-redux';
+import { updateUser } from './redux/user_reducer';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <HashRouter>
-      <div id='App'>
-        <header id='header'>
-          <nav id='header-text'>
-            <h2><Link
-              to='/'
-            >Cardhelper</Link></h2>
-            <h2 id='login-logout-button'><Link
-              to='/auth'
-            >Login</Link></h2>
-          </nav>
-        </header>
-        <main id='main-content'>
-          {routes}
-        </main>
-      </div>
-    </HashRouter>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    axios
+      .get('/auth/session')
+      .then(user => {
+        this.props.updateUser(user.data);
+      })
+  }
+
+  logout() {
+    axios
+      .get('/auth/logout')
+      .then(() => {
+        this.props.updateUser(null);
+        this.props.history.push('/');
+      })
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <div id='App'>
+          <header id='header'>
+            <nav id='header-text'>
+              <h2><Link
+                to='/'
+              >Cardhelper</Link></h2>
+              <h2 id='login-logout-button'><Link
+                to='/auth'
+              >Login</Link></h2>
+            </nav>
+          </header>
+          <main id='main-content'>
+            {routes}
+          </main>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => state
+
+export default withRouter(connect(mapStateToProps, {updateUser})(App));
