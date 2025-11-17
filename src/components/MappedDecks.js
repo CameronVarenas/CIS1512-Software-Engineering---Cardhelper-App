905b0e8b9534f5584767126e5d7336d2b2a15a06
Cameron Varenas <camerontvarenas@gmail.com>
2025-11-17 13:34:44 -0500

    build out userDecks.js and mappedDecks.js

---
+++ src/components/MappedDecks.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MappedDecks(props) {
+    const [deckNewName, setDeckNewName] = useState('');
+
+    const handleDeckNewNameInput = (e) => {
+        setDeckNewName(e.target.value);
+    }
+    
+    return (
+        <section id='deck-display'>
+            <div>
+                <h4>{props.deck.name}</h4>
+            </div>
+            <div>
+                <Link
+                    to={`/flashcard/${props.deck.deck_id}`}
+                    className='deck-options'
+                >Study</Link>
+                <Link
+                    to={`/add-card/${props.deck.deck_id}`}
+                    className='deck-options'
+                >Add Card</Link>
+                <Link
+                    to={`/card-list/${props.deck.deck_id}`}
+                    className='deck-options'
+                >View Cards</Link>
+                <button
+                    className='deck-options'
+                    onClick={() => props.renameDeck(props.deck.deck_id, deckNewName)}
+                >Rename Deck</button>
+                <button
+                    className='deck-options'
+                    onClick={() => props.deleteDeck(props.deck.deck_id)}
+                >Delete Deck</button>
+            </div>
+            <input
+                type='text'
+                placeholder='Enter a new name'
+                onChange={e => handleDeckNewNameInput(e)}
+                value={deckNewName}
+            ></input>
+        </section>
+    )
+};
---
+++ src/components/UserDecks.js
@@ -0,0 +1,106 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import MappedDecks from './MappedDecks';
+
+class UserDecks extends Component {
+    constructor() {
+        super();
+        this.state = {
+            deckName: '',
+            userDecks: []
+        }
+        this.deleteDeck = this.deleteDeck.bind(this);
+        this.renameDeck = this.renameDeck.bind(this);
+    }
+
+    componentDidMount() {
+        if (!this.props.user) {
+            return this.props.history.push('/')
+        }
+        this.getDecks();
+    }
+   
+    handleDeckNameInput(e) {
+        this.setState({deckName: e.target.value});
+    }
+
+    getDecks() {
+        axios
+            .get('/api/decks/')
+            .then((res) => {
+                this.setState({userDecks: res.data});
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }
+
+    createDeck() {
+        const {deckName} = this.state
+        axios
+            .post('/api/decks/', {deckName})
+            .then(() => {
+                this.setState({deckName: ''});
+                this.getDecks();
+            })
+            .catch((error) => {
+                alert(error.response.request.response)
+            })
+    }
+
+    renameDeck(deck_id, deckNewName) {
+        axios
+            .put(`/api/decks/${deck_id}`, {deckNewName})
+            .then(() => this.getDecks())
+            .catch((error) => {
+                console.log(this)
+                alert(error)
+            })
+    }
+
+    deleteDeck(deck_id) {
+        axios
+            .delete(`/api/decks/${deck_id}`)
+            .then(() => this.getDecks())
+            .catch((error) => {
+                alert(error.response.request.response)
+            })
+    }
+
+    render() {
+        const decksMapped = this.state.userDecks.map(deck => {
+            return <MappedDecks
+                deck={deck}
+                deleteDeck={this.deleteDeck}
+                renameDeck={this.renameDeck}
+            />
+        })
+
+            return (
+                <div>
+                    <section>
+                        <h3>Your Decks</h3>
+                    </section>
+                    <section>
+                        <input
+                            className='user-decks-inputs'
+                            type='text'
+                            placeholder='Enter deck name'
+                            onChange={e => this.handleDeckNameInput(e)}
+                            value={this.state.deckName}
+                        ></input>
+                        <button
+                            className='user-decks-inputs'
+                            onClick={() => this.createDeck()}
+                        >Create New Deck</button>
+                    </section>
+                    {decksMapped}
+                </div>
+            )
+    }
+}
+
+const mapStateToProps = state => state
+
+export default connect(mapStateToProps)(UserDecks)
