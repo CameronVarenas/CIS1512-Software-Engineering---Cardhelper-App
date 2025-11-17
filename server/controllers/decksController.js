d16f51c91850f01bfc4806cf9ef9cab72f57e5c0
Cameron Varenas <camerontvarenas@gmail.com>
2025-11-17 12:26:13 -0500

    build out cardController and deckController

---
+++ server/controllers/decksController.js
@@ -0,0 +1,36 @@
+module.exports = {
+    getUserDecks: async (req, res) => {
+        const db = req.app.get('db');
+        const result = await db.decks.get_all_users_decks(req.session.user.id);
+        return res.status(201).send(result);
+    },
+
+    getOneDeck: async (req, res) => {
+        const {deck_id} = req.params;
+        const db = req.app.get('db');
+        const result = await db.decks.get_one_users_decks([req.session.user.id, deck_id]);
+        return res.status(201).send(result);
+    },
+
+    addDeck: async (req, res) => {
+        const {deckName} = req.body;
+        const db = req.app.get('db');
+        const result = await db.decks.add_deck_to_user([deckName, req.session.user.id]);
+        return res.status(201).send(result);
+    },
+
+    updateDeck: async (req, res) => {
+        const {deck_id} = req.params;
+        const {deckNewName} = req.body;
+        const db = req.app.get('db');
+        await db.decks.rename_users_deck([deckNewName, deck_id]);
+        return res.sendStatus(201);
+    },
+
+    deleteDeck: async (req, res) => {
+        const {deck_id} = req.params;
+        const db = req.app.get('db');
+        const result = await db.decks.delete_users_deck([deck_id, req.session.user.id]);
+        return res.status(201).send(result);
+    }
+};
