185013edb13ecfd68db86a2af4c8c5f98fe83d37
Cameron Varenas <camerontvarenas@gmail.com>
2025-11-24 19:15:40 -0500

    re-structure mappedDecks component

--- src/components/MappedDecks.js
+++ src/components/MappedDecks.js
@@ -3,42 +3,90 @@ import { Link } from 'react-router-dom';
 
 export default function MappedDecks(props) {
     const [deckNewName, setDeckNewName] = useState('');
+    const [showRename, setShowRename] = useState(false);
+    const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
 
     const handleDeckNewNameInput = (e) => {
         setDeckNewName(e.target.value);
     }
-    
+
+    const toggleRename = () => {
+        setShowRename(!showRename);
+        setShowDeleteConfirm(false); // close delete section if open
+    }
+
+    const toggleDeleteConfirm = () => {
+        setShowDeleteConfirm(!showDeleteConfirm);
+        setShowRename(false); // close rename section if open
+    }
+
     return (
-        <section id='deck-display'>
-            <h4 id='deck-name'>{props.deck.name}</h4>
-            <div id='deck-options-bar'>
+        <div>
+            <section id='deck-options-bar'>
+                <h4 id='deck-name'>{props.deck.name}</h4>
                 <Link
                     to={`/flashcard/${props.deck.deck_id}`}
                     className='deck-options'
                 >Study</Link>
+
                 <Link
                     to={`/add-card/${props.deck.deck_id}`}
                     className='deck-options'
                 >Add Card</Link>
+
                 <Link
                     to={`/card-list/${props.deck.deck_id}`}
                     className='deck-options'
                 >View Cards</Link>
+
                 <button
                     className='deck-options'
-                    onClick={() => props.renameDeck(props.deck.deck_id, deckNewName)}
+                    onClick={toggleRename}
                 >Rename Deck</button>
+
                 <button
                     className='deck-options'
-                    onClick={() => props.deleteDeck(props.deck.deck_id)}
+                    onClick={toggleDeleteConfirm}
                 >Delete Deck</button>
-            </div>
-            {/* <input
-                type='text'
-                placeholder='Enter a new name'
-                onChange={e => handleDeckNewNameInput(e)}
-                value={deckNewName}
-            ></input> */}
-        </section>
-    )
-};
+            </section>
+
+            <section>
+                {/* RENAME SECTION */}
+                {showRename && (
+                    <div className="rename-section">
+                        <input
+                            type='text'
+                            placeholder='Enter a new deck name'
+                            onChange={handleDeckNewNameInput}
+                            value={deckNewName}
+                        />
+                        <button
+                            onClick={() => {
+                                props.renameDeck(props.deck.deck_id, deckNewName);
+                                setDeckNewName('');
+                                setShowRename(false);
+                            }}
+                        >
+                            Save Name
+                        </button>
+                    </div>
+                )}
+
+
+                {/* DELETE CONFIRM SECTION */}
+                {showDeleteConfirm && (
+                    <div className="delete-confirm-section">
+                        <p>Are you sure you want to delete this deck?</p>
+                        <button
+                            className="delete-button"
+                            onClick={() => props.deleteDeck(props.deck.deck_id)}
+                        >
+                            Delete Deck
+                        </button>
+                    </div>
+                )}
+            </section>
+
+        </div>
+    );
+}
